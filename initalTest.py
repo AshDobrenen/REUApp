9446f7de05430a41245d9edc348a1f68cdbc1309
Ash Dobrenen <108023087+AshDobrenen@users.noreply.github.com>
2022-07-14 14:02:43 -0400

    Update initalTest.py

--- initalTest.py
+++ initalTest.py
@@ -1,1 +1,24 @@
 from imports import *
+#this is to test the opening of the website file, finding the data-attribute code, and exporting it to a csv. 
+#after I will add a new file to do this for all the test websites to one csv, and all the training websites to another
+###########
+#opening and reading the website
+
+
+###########
+#extracting and saving sata-attributes to database
+
+###########
+#exporting to a csv
+#headers = item, features, code
+listOfHeaders = []
+savePath = 
+fileName = "initalTest.csv"
+fullName = os.path.join(savePath, fileName)
+
+with open(fullName, "a", newline="") as c:
+  writer = csv.writer(f)
+  writer.writerow(listOfHeaders)
+ 
+#writing out the content of the database
+
