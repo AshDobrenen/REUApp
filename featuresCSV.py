9209028ada2102e1825ad9e9ad35bf63a77e4751
Ash Dobrenen <108023087+AshDobrenen@users.noreply.github.com>
2022-08-02 14:10:04 -0400

    Update featuresCSV.py

--- featuresCSV.py
+++ featuresCSV.py
@@ -9,8 +9,6 @@ import requests
 import sys
 import re
 import os
-#this is to test the opening of the website file, finding the data-attribute code, and exporting it to a csv. 
-#after I will add a new file to do this for all the test websites to one csv, and all the training websites to another
 
 ###########
 #open and then extracting data-attributes and saving all to database
@@ -18,32 +16,15 @@ def getDataPoints(turls):
   item, pAlt, pKeyword, redirect, pPicText, picTextLength, picKeywords, pAdSymbol, lengthAlt, pVideoTag, code = ([] for n in range(11))
   tFile = open(turls, 'r', encoding="latin-1")
   itemString = []
-  #ad in what values each feature can have, use 0 or 1 rather than true or false
   readFile = tFile.read()
   #check beautiful soup version = 4.11.1
   
   soup = BeautifulSoup(readFile, "html.parser")
-  #print(soup.html)
   with open('readme.txt', 'w', encoding = 'utf-8') as f:
-    
-     f.write(str(soup.prettify()))
-   #read up on traversal
-  #for child in soup.recursiveChildGenerator():
-
-       # if child.hasattr("data-attribute"):
-            
-            #print(child.name)
-  #print(soup.prettify())
-  #get all of data attributes in the soup
-  #loop through it? only getting first instance of attr
-  #item = soup.get('data-attribute')#data-attribute="deceptive" or nondeceptive
-  #item = soup.find_all('a')
-  item = soup.select('[data-attribute]')
-  #print(item)
-  #print(len(item))
+    f.write(str(soup.prettify()))
   
-  #create a table that will be filled in and returnd to the main function
-
+  item = soup.select('[data-attribute]')
+  
   #fill in the features and code to the lists individually
   for i in range(0,len(item)):
       itemString.append(str(item[i]))
@@ -56,46 +37,23 @@ def getDataPoints(turls):
           pAlt.append(0)
           lengthAlt.append(0)
       #presence of ad, advertisement, paid, sponsered, Facebook, Twitter, Instagram, TikTok
-      #convert item to string, then can search in string, look up how to do this
-      #finding ad as a partial of a word
-      #reg expression of not any other letter before or after ad, so that it can have characters or spaces, but not letters
-      #change to some sort of list since if is so long?
       if(re.search("[^a-zA-Z]ad[^a-zA-Z]|[^a-zA-Z]advertisement[^a-zA-Z]|[^a-zA-Z]paid[^a-zA-Z]|[^a-zA-Z]sponsered[^a-zA-Z]|[^a-zA-Z]Facebook[^a-zA-Z]|[^a-zA-Z]Twitter[^a-zA-Z]|[^a-zA-Z]Instagram[^a-zA-Z]|[^a-zA-Z]TikTok[^a-zA-Z]|[^a-zA-Z]Linkedin[^a-zA-Z]|[^a-zA-Z]search[^a-zA-Z]|[^a-zA-Z]google plus[^a-zA-Z]|[^a-zA-Z]reddit[^a-zA-Z]|[^a-zA-Z]email[^a-zA-Z]|[^a-zA-Z]youtube[^a-zA-Z]", itemString[i])!=None):
           pKeyword.append(1)
       else:
           pKeyword.append(0)
  
-    ##############################
- #will need other libraris, look later
-      #redirect url idk
-      
-      #picture text based ones besides alt text idk, doesnt quite work, but somtheing like this
-      #image = item[i].find('img')
-      #picSource = image.attrs['src']
-     # print(picSource)
-      #####################################
-      
       #video tag, will be done the same way as keywords
       if(itemString[i].find("video")>=0):
           pVideoTag.append(1)
       else:
           pVideoTag.append(0)
   
-      #da == dec, code = 1 and vice versa, same as keywords, convert to string, search string for full data attr
+      #da == dec, code = 1 and vice versa, same as keywords, search string for full data attr
       if(itemString[i].find('data-attribute="deceptive"')>=0):
          code.append(1)
       else:
          code.append(0)
   
-  
-  #make to a dataframe from dictionaires, then push the dataframe to csv
-  #seperate lists of each feature, these will become columns
-  #then dictionairy of featuresTable
-  #featuresTable = {"pAlt": pAlt, "pKeyword": pKeyword, "lengthAlt": lengthAlt, "pVideoTag": pVideoTag, "code": code}
-  #1 is true/deceptive 0 is false/nondeceptive
-  #print(featuresTable)
-  
-  #featuresDF = pd.DataFrame(featuresTable)
   
   return  pAlt, pKeyword, lengthAlt, pVideoTag, code
 
@@ -196,16 +154,9 @@ for n in range(0,54):
             lengthAlt.extend(testTable2[2])
             pVideoTag.extend(testTable2[3])
             code.extend(testTable2[4])
-"""           
-print(pAlt)
-print(pKeyword)
-print(lengthAlt)
-print(pVideoTag)
-print(code)
-"""            
+         
 ###########
 #write to csv               
 dict = {"pAlt": pAlt, "pKeyword": pKeyword, "lengthAlt": lengthAlt, "pVideoTag": pVideoTag, "code": code}
 df = DataFrame(dict)
-#print(df)
 df.to_csv('initalTest.csv')
